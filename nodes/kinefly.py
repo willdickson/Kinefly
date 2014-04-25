b23dc18cd1247cfe31eb0e1d17c610f4b55ff1f8
Steve Safarik <ssafarik@uw.edu>
2014-04-25 14:26:12 -0700

    Handle div by zero, and change handle size.

--- nodes/kinefly.py
+++ nodes/kinefly.py
@@ -74,8 +74,13 @@ def get_intersection(pt1a, pt1b, pt2a, pt2b):
     x4 = pt2b[0]
     y4 = pt2b[1]
     
-    x = (x1*x3*y2 - x2*x3*y1 - x1*x4*y2 + x2*x4*y1 - x1*x3*y4 + x1*x4*y3 + x2*x3*y4 - x2*x4*y3)/(x1*y3 - x3*y1 - x1*y4 - x2*y3 + x3*y2 + x4*y1 + x2*y4 - x4*y2)
-    y = (x1*y2*y3 - x2*y1*y3 - x1*y2*y4 + x2*y1*y4 - x3*y1*y4 + x4*y1*y3 + x3*y2*y4 - x4*y2*y3)/(x1*y3 - x3*y1 - x1*y4 - x2*y3 + x3*y2 + x4*y1 + x2*y4 - x4*y2)        
+    den = (x1*y3 - x3*y1 - x1*y4 - x2*y3 + x3*y2 + x4*y1 + x2*y4 - x4*y2)
+    if (den != 0.0):
+        x = (x1*x3*y2 - x2*x3*y1 - x1*x4*y2 + x2*x4*y1 - x1*x3*y4 + x1*x4*y3 + x2*x3*y4 - x2*x4*y3) / den
+        y = (x1*y2*y3 - x2*y1*y3 - x1*y2*y4 + x2*y1*y4 - x3*y1*y4 + x4*y1*y3 + x3*y2*y4 - x4*y2*y3) / den
+    else:
+        x = x3   
+        y = y3   
     
     return np.array([x,y])
             
@@ -330,7 +335,7 @@ class Handle(object):
         self.scale = 1.0
 
         self.color = color
-        self.radius = 4
+        self.radius = 3
 
 
     def hit_test(self, ptMouse):
